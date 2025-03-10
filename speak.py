37466b510d2e67934b51f873ea6421438a8fda17
deep9999 <deepdholariya60@gmail.com>
2025-03-10 15:50:22 +0530

    Update speak.py

--- speak.py
+++ speak.py
@@ -1,13 +1,7 @@
-import win32com.client as win
+import win32com.client
 
-speaker = win.Dispatch("SAPI.SpVoice")
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
 
-names_list = ["Deep", "Dp", "Deep9999"]
-
-for name in names_list:
-    print("Shoutout to " + name)
-    shoutout = f"Shoutout to {name}"
-    speaker.Speak(shoutout)
-
-print("Shoutout to all the guys")
-speaker.Speak("Shoutout to all the guys")
+print("Enter the word you want to speak ")
+s = input()
+speaker.Speak(s)
